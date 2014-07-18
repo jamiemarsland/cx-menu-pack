3a8e2a8b8b0c4812b8a1c4fbf38a33aa0d8124cf
Alan Ng <jenfoong@gmail.com>
2014-07-18 16:46:18 +0800

    Add Updater

--- pootlepress-menu-pack.php
+++ pootlepress-menu-pack.php
@@ -28,8 +28,21 @@ License: GPL version 2 or later - http://www.gnu.org/licenses/old-licenses/gpl-2
 
 	require_once( 'pootlepress-menu-pack-functions.php' );
 	require_once( 'classes/class-pootlepress-menu-pack.php' );
+    require_once( 'classes/class-pootlepress-updater.php');
 
     $GLOBALS['pootlepress_menu_pack'] = new Pootlepress_Menu_Pack( __FILE__ );
     $GLOBALS['pootlepress_menu_pack']->version = '1.0.0';
-	
+
+add_action('init', 'pp_mp_updater');
+function pp_mp_updater()
+{
+    if (!function_exists('get_plugin_data')) {
+        include(ABSPATH . 'wp-admin/includes/plugin.php');
+    }
+    $data = get_plugin_data(__FILE__);
+    $wptuts_plugin_current_version = $data['Version'];
+    $wptuts_plugin_remote_path = 'http://www.pootlepress.com/?updater=1';
+    $wptuts_plugin_slug = plugin_basename(__FILE__);
+    new Pootlepress_Updater ($wptuts_plugin_current_version, $wptuts_plugin_remote_path, $wptuts_plugin_slug);
+}
 ?>
